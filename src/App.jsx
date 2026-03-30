fd962188dc06a95ff65a9b8fdfb93c407435b013
cynotraquemoselle <cynotraque.moselle@gmail.com>
2026-03-30 11:43:21 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div style={{fontFamily: "Arial", padding: 40}}>
+      <img src="/logo.png" style={{width: 150}} />
+
+      <h1>Cynotraque Moselle</h1>
+      <p>Traque, chiens et territoire</p>
+
+      <h2>Association de traqueurs en Moselle</h2>
+
+      <p>
+        Une équipe engagée, organisée et passionnée au service du terrain.
+      </p>
+
+      <h3>Contact</h3>
+      <p>Email : contact@cynotraque-moselle.fr</p>
+    </div>
+  );
+}
